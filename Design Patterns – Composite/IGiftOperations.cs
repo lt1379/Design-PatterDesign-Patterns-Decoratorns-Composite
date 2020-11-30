65af70b052f0230c846a8a09ffbbb9c0adbc4dc7
lamboktulus1379 <lamboktulus1379@gmail.com>
2020-12-01 06:46:12 +0700

    Add project files.

---
+++ Design Patterns – Composite/IGiftOperations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Design_Patterns___Composite
+{
+    public interface IGiftOperations
+    {
+        void Add(GiftBase gift);
+        void Remove(GiftBase gift);
+    }
+}
---
+++ Design Patterns – Composite/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Design_Patterns___Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var phone = new SingleGift("Phone", 256);
+            phone.CalculateTotalPrice();
+            Console.WriteLine();
+
+            // Composite gift
+            var rootBox = new CompositeGift("RootBox", 0);
+            var truckBoy = new SingleGift("TruckToy", 289);
+            var plainToy = new SingleGift("PlainToy", 587);
+            rootBox.Add(truckBoy);
+            rootBox.Add(plainToy);
+            var childBox = new CompositeGift("ChildBox", 0);
+            var soldierToy = new SingleGift("SoldierToy", 200);
+            childBox.Add(soldierToy);
+            rootBox.Add(childBox);
+
+            Console.WriteLine($"Total price of this composite present is: {rootBox.CalculateTotalPrice()}");
+        }
+    }
+}
